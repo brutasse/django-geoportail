393df62883bb387a71507cfc7ed9c91acb9bc1fe
Bruno Renié <buburno@gmail.com>
2010-08-24 22:07:22 +0100

    0.4.4 is out

--- geoportal/__init__.py
+++ geoportal/__init__.py
@@ -1,12 +1,7 @@
 from geoportal import utils, admin, templatetags, forms
 
-VERSION = (0, 4, 3)
+VERSION = (0, 4, 4)
 
-
-def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if len(VERSION) > 2:
-        version = '%s.%s' % (version, VERSION[2])
-    return version
+get_version = lambda: '.'.join(map(str, VERSION))
 
 __all__ = ['utils', 'admin', 'templatetags', 'forms']
